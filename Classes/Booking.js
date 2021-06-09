d22d055791f54516a15ebbb70c2a7f43b3f2a3e8
Beth <bethm.meeker@gmail.com>
2021-06-09 17:49:38 -0600

    Write Booking tests and Booking class

--- Classes/Booking.js
+++ Classes/Booking.js
@@ -1,7 +1,21 @@
 class Booking {
-  constructor() {
-      
+  constructor(newBooking) {
+    this.id = newBooking.id;
+    this.userID = newBooking.userID;
+    this.date = newBooking.date;
+    this.roomNumber = newBooking.roomNumber;
+    this.roomServiceCharges = [];
   }
+
+  // method to find customer info
+
+  // method to block the room from availability for booked dates
+
+  // method to total room service charges
+
+  // method to total the room total for booked nights 
+
+  // method to total all charges at checkout
 }
 
 
--- test/Booking-test.js
+++ test/Booking-test.js
@@ -4,41 +4,42 @@ import sampleBookingsData from '../SampleData/sample-bookings';
 import Booking from '../Classes/Booking';
 // import Room from '../Classes/Room';
 
-let booking1, booking2, booking3, booking4, booking5;
+let booking1, booking2, booking3, booking4, booking5, booking6;
 
 describe('Booking', () => {
 
   beforeEach(() => {
-    booking1 = new Booking(sampleBookingsData[0]);
-    booking2 = new Booking(sampleBookingsData[1]);
-    booking3 = new Booking(sampleBookingsData[2]);
-    booking4 = new Booking(sampleBookingsData[3]);
-    booking5 = new Booking(sampleBookingsData[4]);
+    booking1 = new Booking(sampleBookingsData.bookings[0]);
+    booking2 = new Booking(sampleBookingsData.bookings[1]);
+    booking3 = new Booking(sampleBookingsData.bookings[2]);
+    booking4 = new Booking(sampleBookingsData.bookings[3]);
+    booking5 = new Booking(sampleBookingsData.bookings[4]);
+    booking6 = new Booking(sampleBookingsData.bookings[5]);
   });
 
   it('should be an instance of Booking', function() {
     expect(booking1).to.be.instanceOf(Booking);
   });
 
-  it.skip('should have an id', function() {
+  it('should have an id', function() {
     expect(booking2.id).to.be.a('string');
     expect(booking2.id).to.equal("5fwrgu4i7k55hl71t");
   });
 
-  it.skip('should know what userID (customer) booked the room', function() {
-    expect(booking5.userID).to.equal(12);
+  it('should know what userID (customer) booked the room', function() {
+    expect(booking6.userID).to.equal(12);
   });
 
-  it.skip('should have a booking date', function() {
+  it('should have a booking date', function() {
     expect(booking3.date).to.equal("2020/01/10");
   });
 
-  it.skip('should know what room number was booked', function() {
+  it('should know what room number was booked', function() {
     expect(booking4.roomNumber).to.be.a('number');
     expect(booking4.roomNumber).to.equal(15);
   });
 
-  it.skip('should keep track of room service charges', function() {
+  it('should keep track of room service charges', function() {
     expect(booking1.roomServiceCharges).to.be.an('array');
     expect(booking1.roomServiceCharges.length).to.be.equal(0);
   });
