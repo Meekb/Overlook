4c304cc62401906ae7772b03727ee27276c3a374
Beth <bethm.meeker@gmail.com>
2021-06-09 14:28:47 -0600

    Save apiCalls as is for now

---
+++ Classes/Customer.js
@@ -0,0 +1,7 @@
+class Customer {
+  constructor(newCustomer) {
+    this.id = newCustomer.id;
+    this.name = newCustomer.name;
+    console.log(newCustomer);
+  }
+}
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,26 +1,27 @@
 let getData = (pathEnd) => fetch(`http://localhost:3001/api/v1/${pathEnd}`)
-    .then(response => response.JSON())
-    .catch(err => console.log('error', err));
+  .then(response => response.json())
+  .catch(err => console.log('error', err))
+
 
 function getAllData() {
     return Promise.all([getData('customers'), getData(`customers/${id}`), getData('rooms'), getData('bookings')]);
 }
 
-let dataSend = (sentData, url) => {
-    return fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(sentData),
-        headers: {
-            'Content-type': 'application/json'
-        }
-    });
-    .then(response => response.JSON())
-    .then(JSON => console.log('This is JSON response from API', JSON));
-    .catch(err => console.log(err));
-}
+// let dataSend = (sentData, url) => {
+//     return fetch(url, {
+//         method: 'POST',
+//         body: json.stringify(sentData),
+//         headers: {
+//             'Content-type': 'application/json'
+//         }
+//     });
+//     .then(response => response.json());
+//     .then(json => console.log('This is JSON response from API', json));
+//     .catch(err => console.log(err));
+// }
 
 let dataToPost = (postData, pathEnd) => {
     return Promise.all([sendData(postData, `http://localhost:3001/api/v1/${pathEnd}`)]);
 }
 
-export default { getAllData, dataToPost };
+export default { getAllData };
