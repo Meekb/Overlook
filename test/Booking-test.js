48d3cd426a5125c9f1dd3f2a4260727875464d14
Beth <bethm.meeker@gmail.com>
2021-06-09 17:40:32 -0600

    Write tests for Booking class

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -21,13 +21,29 @@ describe('Booking', () => {
   });
 
   it.skip('should have an id', function() {
+    expect(booking2.id).to.be.a('string');
     expect(booking2.id).to.equal("5fwrgu4i7k55hl71t");
-  })
+  });
 
+  it.skip('should know what userID (customer) booked the room', function() {
+    expect(booking5.userID).to.equal(12);
+  });
 
+  it.skip('should have a booking date', function() {
+    expect(booking3.date).to.equal("2020/01/10");
+  });
 
+  it.skip('should know what room number was booked', function() {
+    expect(booking4.roomNumber).to.be.a('number');
+    expect(booking4.roomNumber).to.equal(15);
+  });
 
+  it.skip('should keep track of room service charges', function() {
+    expect(booking1.roomServiceCharges).to.be.an('array');
+    expect(booking1.roomServiceCharges.length).to.be.equal(0);
+  });
 
-
+  // other tests/methods TBD
+  // will need to add more tests for roomServiceCharges (class Service still needed)
     
 });
